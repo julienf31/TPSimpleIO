102d00312c02926e6e99d5ce286012494ba59832
Julien <julien.fournier@ynov.com>
2017-10-17 15:17:44 +0200

    Up

--- Ex4/src/PrintIntoFile.java
+++ Ex4/src/PrintIntoFile.java
@@ -1,17 +1,15 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class PrintIntoFile {
 
-    static File file = new File("./TPSimpleIO/Ex4/file/file.txt");
+    static File file = new File("./TPSimpleIO/Ex4/resources/file.txt");
 
     public static void main(String[] args){
         String s = "";
         Scanner scan = new Scanner(System.in);
         while(!s.equals("quit")){   //boucle tant que quit n'est pas tapé
+            System.out.println("Saisissez la phrase à copier dans le fichier : (file.txt)");
             s = scan.nextLine();    //récupération de la saisie
             if(!s.equals("quit")){   //si non quit, on écrit dans le fichier
                 // écriture dans le fichier (avec exception), append pour ajouter a la suite et ne pas réécrire par dessus
